e9713017cd562065e336803cfce1e0b08d059355
nberpin <nberpin@canariaseducacion.es>
2023-09-14 17:04:08 +0100

    primera clase

---
+++ 01-hola-mundo.php
@@ -0,0 +1,11 @@
+
+<?php include 'includes/header.php';
+    echo "<h1>Hola mundo! </h1>";
+    //otras funciones para mostrar print y print_r
+
+    //la otra función que vamos a utilizar para mostrar datos es var_dump, para qué crees que la usaremos?
+    var_dump("hola mundo");
+
+    include 'includes/footer.php';
+
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+
+<?php
+    echo "<h1>Hola mundo! </h1>";
+    //otras funciones para mostrar print y print_r
+
+    //la otra función que vamos a utilizar para mostrar datos es var_dump, para qué crees que la usaremos?
+    var_dump("hola mundo");
+
+
+
+?>
